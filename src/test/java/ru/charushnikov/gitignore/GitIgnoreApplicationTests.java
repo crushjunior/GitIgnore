ac315fc271f657cba2e5518ce7bf6c2d7b7c56c5
crushjunior <charushnikovvadim@gmail.com>
2023-10-19 21:19:47 +0300

    Initial commit

---
+++ src/test/java/ru/charushnikov/gitignore/GitIgnoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.charushnikov.gitignore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIgnoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
